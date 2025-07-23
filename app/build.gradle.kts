26513b23f86ea98f9545d1410c11cd43ad494331
sarim2000 <sarimbleedblue@gmail.com>
2025-07-23 11:08:21 +0530

    chore: downgrade minSdk version from 34 to 31 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = "com.pennywiseai.tracker"
-        minSdk = 34
+        minSdk = 31
         targetSdk = 35
         versionCode = 2
         versionName = "1.0.1"
