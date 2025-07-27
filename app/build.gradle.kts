12df7a1eb4dfad833adca3ce7973e16bea6b6202
sarim2000 <sarimbleedblue@gmail.com>
2025-07-28 00:49:26 +0530

    chore: increment version code to 8 and update version name to 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.pennywiseai.tracker"
         minSdk = 31
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.4"
+        versionCode = 8
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
