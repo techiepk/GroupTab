af2bf992bfbb2fa1743ef6c729675bd64b214eae
sarim2000 <sarimbleedblue@gmail.com>
2025-08-07 20:27:30 +0530

    Update app version to 2.1.4 (versionCode: 14) in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.pennywiseai.tracker"
         minSdk = 31
         targetSdk = 36
-        versionCode = 13
-        versionName = "2.1.3"
+        versionCode = 14
+        versionName = "2.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
