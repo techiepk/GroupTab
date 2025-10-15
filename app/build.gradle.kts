131eb0077a9cf181b9a1d0e87e5f2e80ead31f02
sarim2000 <sarimbleedblue@gmail.com>
2025-10-15 12:15:12 +0530

    chore(release): bump version to 2.15.36 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.pennywiseai.tracker"
         minSdk = 30
         targetSdk = 36
-        versionCode = 70
-        versionName = "2.15.35"
+        versionCode = 71
+        versionName = "2.15.36"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
